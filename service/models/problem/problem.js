ed3939791a4d229c6cf1d1ab6f84e01215a691e3
jvalderlei <vander.vander@gmail.com>
2015-07-07 19:05:50 -0300

    Modelo Problem.

---
+++ service/models/problem/problem.js
@@ -0,0 +1,147 @@
+/**
+ * Created by JOSEVALDERLEI on 07/07/2015.
+ */
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+
+var StakeholderSchema = new Schema({
+    name: {
+        type: String,
+        required:'required.name',
+        trim: true
+    },
+
+    onionlayer: {
+        type: String,
+        requried: 'required.layer',
+        trim: true
+    },
+
+    description: {
+        type: String,
+        required: 'required.description',
+        trim: true
+    },
+
+    positionX: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    positionY: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    openEdit:{
+        type: boolean,
+        required: true,
+        default: false
+    },
+
+    evaluationFraming: {
+        problemsSolutions: [{
+            problem: {
+                type: String,
+                required: "required.problem",
+                trim: true
+            },
+            solution: {
+                type: String,
+                required: "required.solution",
+                trim: true
+            }
+        }]
+    },
+
+    values: [String]
+
+});
+
+var SemioticFrameworkSchema = new Schema({
+    socialWorlds: [{
+        requirement: String,
+        restriction: String
+    }],
+
+    pragmatic: [{
+        requirement: String,
+        restriction: String
+    }],
+
+    semantic: [{
+        requirement: String,
+        restriction: String
+    }],
+
+    syntatic: [{
+        requirement: String,
+        restriction: String
+    }],
+
+    empirical: [{
+        requirement: String,
+        restriction: String
+    }],
+
+    physical: [{
+        requirement: String,
+        restriction: String
+    }]
+});
+
+var CultureAwareRequirementsFrameworkSchema = new  Schema({
+    field: {
+        type: String,
+        requried: 'requried.field',
+        trim: true
+    },
+
+    value: {
+        type: String,
+        required: 'required:value',
+        trim: true
+    },
+
+    requirement: {
+        type: String,
+        required: 'required.requirement',
+        trim: true
+    },
+
+    stakeholders: [String],
+
+    priority: {
+        type: String,
+        enum: ['LOW', 'MEDIUM', 'HIGH']
+    }
+
+
+});
+
+var ProblemaBodySchema = new Schema({
+    title: {
+        type: String,
+        required: 'required.title',
+        trim: true
+    },
+
+    description: {
+        type: String,
+        required: 'required.description',
+        trim: true
+    },
+       
+
+    stakeholders: [StakeholderSchema],
+
+    semioticFramework: [SemioticFrameworkSchema],
+
+    values: [String],
+
+    cultureAwareRequirementsFramework: [CultureAwareRequirementsFrameworkSchema]
+});
+
+module.exports = mongoose.model('ProblemBody', ProblemaBodySchema);
