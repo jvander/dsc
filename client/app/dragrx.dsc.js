5cb264230317731410e3074d669fe7bebb61ee13
jesus <vander.vander@gmail.com>
2016-03-30 15:57:02 -0300

    Ajustando dragX.

---
+++ client/app/dragrx.dsc.js
@@ -0,0 +1,43 @@
+/**
+ * Created by jesus on 21/03/2016.
+ */
+;(function (undefined) {
+    angular.module('appRxDrag', ['rx'])
+        .directive('rxdraggable', function($document, $window, rx, Socket) {
+            return function(scope, element, attrs) {
+                // Get the three major events
+                var localcode =  $window.localStorage.getItem('localcode');
+                var mouseup   = rx.Observable.fromEvent(element,   'mouseup');
+                var mousemove = rx.Observable.fromEvent($document, 'mousemove');
+                var mousedown = rx.Observable.fromEvent(element,   'mousedown').map(function (event) {
+                    // calculate offsets when mouse down
+                    event.preventDefault();
+                    return {
+                        left: event.clientX - element[0].getBoundingClientRect().left,
+                        top:  event.clientY - element[0].getBoundingClientRect().top,
+                    };
+                });
+
+                // Combine mouse down with mouse move until mouse up
+                var mousedrag = mousedown.selectMany(function(imageOffset) {
+                    return mousemove.map(function (pos) {
+                        // calculate offsets from mouse down to mouse moves
+                        return {
+                            left: pos.clientX - imageOffset.left,
+                            top:  pos.clientY - imageOffset.top
+                        };
+                    }).takeUntil(mouseup);
+                });
+
+                mousedrag.subscribe (function (pos) {
+                    // Update position
+                    element.css({top: pos.top + 'px', left: pos.left + 'px'});
+                    pos.localcode = localcode;
+                    Socket.emit('broadcastMove',pos);
+
+                });
+
+            };
+        });
+
+}.call(this));
